688489f0a29786a90ef2ba0894fe3313fc5baed3
2593828650 <2593828650@qq.com>
2021-05-23 17:30:52 +0800

    first commit

---
+++ scripts/musics.js
@@ -0,0 +1,61 @@
+const music = require("musicinjector");
+
+const mcambient1 = loadMusic("mcambient1");
+const mcambient2 = loadMusic("mcambient2");
+const mcambient3 = loadMusic("mcambient3");
+const mcambient4 = loadMusic("mcambient4");
+const mcambient5 = loadMusic("mcambient5");
+const mcambient6 = loadMusic("mcambient6");
+const mcambient7 = loadMusic("mcambient7");
+const mcambient8 = loadMusic("mcambient8");
+const mcambient9 = loadMusic("mcambient9");
+const mcambient10 = loadMusic("mcambient10");
+const mcambient11 = loadMusic("mcambient11");
+const mcambient12 = loadMusic("mcambient12");
+const mcambient13 = loadMusic("mcambient13");
+const mcambient14 = loadMusic("mcambient14");
+const mcambient15 = loadMusic("mcambient15");
+const mcambient16 = loadMusic("mcambient16");
+const mcambient17 = loadMusic("mcambient17");
+const mcambient18 = loadMusic("mcambient18");
+const mcambient19 = loadMusic("mcambient19");
+
+const mcboss1 = loadMusic("mcboss1");
+const mcboss2 = loadMusic("mcboss2");
+
+const mcdark1 = loadMusic("mcdark1");
+const mcdark2 = loadMusic("mcdark2");
+const mcdark3 = loadMusic("mcdark3");
+const mcdark4 = loadMusic("mcdark4");
+
+music.clearAllTracks("ambientMusic");
+music.clearAllTracks("darkMusic");
+music.clearAllTracks("bossMusic");
+
+music.addTrack(mcambient1, "ambientMusic");
+music.addTrack(mcambient2, "ambientMusic");
+music.addTrack(mcambient3, "ambientMusic");
+music.addTrack(mcambient4, "ambientMusic");
+music.addTrack(mcambient5, "ambientMusic");
+music.addTrack(mcambient6, "ambientMusic");
+music.addTrack(mcambient7, "ambientMusic");
+music.addTrack(mcambient8, "ambientMusic");
+music.addTrack(mcambient9, "ambientMusic");
+music.addTrack(mcambient10, "ambientMusic");
+music.addTrack(mcambient11, "ambientMusic");
+music.addTrack(mcambient12, "ambientMusic");
+music.addTrack(mcambient13, "ambientMusic");
+music.addTrack(mcambient14, "ambientMusic");
+music.addTrack(mcambient15, "ambientMusic");
+music.addTrack(mcambient16, "ambientMusic");
+music.addTrack(mcambient17, "ambientMusic");
+music.addTrack(mcambient18, "ambientMusic");
+music.addTrack(mcambient19, "ambientMusic");
+
+music.addTrack(mcboss1, "bossMusic");
+music.addTrack(mcboss2, "bossMusic");
+
+music.addTrack(mcdark1, "darkMusic");
+music.addTrack(mcdark2, "darkMusic");
+music.addTrack(mcdark3, "darkMusic");
+music.addTrack(mcdark4, "darkMusic");
